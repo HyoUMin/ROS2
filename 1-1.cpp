669d12e38aacbaaa15b2899ea1d7ab2ed51fbb20
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 20:02:01 +0900

    delete

--- 1-1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>   // 입출력 라이브러리 포함
-using namespace std;  // std:: 생략 가능하게 함
-
-int main() {
-    cout << "Hello, Robot!" << endl;  // 화면에 출력
-    return 0;                         // 프로그램 종료
-}
--- 1-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, TurtleBot!" << endl;
-    cout << "System check complete." << endl;
-    cout << "Ready to move!" << endl;
-    return 0;
-}
