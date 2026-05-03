15a672d19777b63d718c7d458a3e902a6b62f4c0
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 20:04:57 +0900

    1. 변수란 무엇인가?

---
+++ 2/2-1.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int distance = 120;
+    double speed = 0.5;
+    string name = "TurtleBot";
+
+    cout << name << " is moving " << distance << "cm at " << speed << " m/s " << endl;
+    return 0; 
+}
