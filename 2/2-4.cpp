b6d14e57f6bdd5c3772c0cb8677be83963928fd8
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 20:07:19 +0900

    6. 형 변환 (Type Casting)

---
+++ 2/2-4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 2;
+    cout << a / b << endl;          // 정수 나눗셈 → 1
+    cout << (double)a / b << endl;  // 실수 나눗셈 → 1.5
+    return 0;
+}
