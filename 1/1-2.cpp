367b937334107a5c35bdbcf5555632a5a68f2263
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 20:01:43 +0900

    4. 실습: 로봇의 인사 메시지 바꿔보기

---
+++ 1/1-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, TurtleBot!" << endl;
+    cout << "System check complete." << endl;
+    cout << "Ready to move!" << endl;
+    return 0;
+}
