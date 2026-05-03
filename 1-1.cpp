1daab6679e435f0423c536c159e9cf34cf28530e
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 19:53:48 +0900

    1. C++ 프로그램의 기본 구조

---
+++ 1-1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   // 입출력 라이브러리 포함
+using namespace std;  // std:: 생략 가능하게 함
+
+int main() {
+    cout << "Hello, Robot!" << endl;  // 화면에 출력
+    return 0;                         // 프로그램 종료
+}
