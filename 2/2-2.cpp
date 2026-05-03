14917f08da425c854ce2ac16b8f06ed77cd10591
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 20:06:05 +0900

    4. 기본 입출력: cin과 cout

---
+++ 2/2-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int distance;
+    cout << "Enter distance (cm): ";
+    cin >> distance;
+    cout << "Distance entered: " << distance << "cm" << endl;
+    return 0;
+}
