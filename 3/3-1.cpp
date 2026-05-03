f669088071c0bea95386cd65abe679a94b2cffda
HyoUMin <yhy0_myn00@naver.com>
2026-05-03 20:21:24 +0900

    1. 조건문 (if 문)

---
+++ 3/3-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool obstacle = false;
+
+    if (obstacle) {
+        cout << "Stop! Obstacle detected." << endl;
+    } else {
+        cout << "Moving forward..." << endl;
+    }
+    return 0;
+}
